a1437576828c90385e34ac81ed8a93246c3543e9
Raina <rcho@elitedigital.ca>
2019-07-31 08:03:02 -0400

    intro to ES2015 and commonJS module syntax wiht app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
-const greeting = 'Hello World';
+// CommonJS Module Syntax
+// const person = require('./mymodule1');
+
+// ES 2015 Module 
+// import {person, sayHello} from './mymodule2';
+// import * as mod from './mymodule2';
+import greeting from './mymodule2';
+
+// console.log(mod.person.age);
+// console.log(mod.sayHello());
 console.log(greeting);
