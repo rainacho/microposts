b027b7f17b21836e6c983c8692a994aaef12e7c7
Raina <rcho@elitedigital.ca>
2019-08-04 01:19:36 -0400

    setup for sass with webpack

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import './app.scss';
 import $ from 'jquery';
 import { http } from './http';
 import { ui } from './ui';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -11,14 +12,36 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Project Demo',
+      minify: {
+       collapseWhitespace: true
+      },
+      hash: true,
+      template: './src/index.html'
+     })
+  ],
   module: {    
-    rules: [{        
-      test: /\.js?$/,        
-      exclude: /node_modules/,        
-      loader: 'babel-loader',        
-      query: {           
-        presets: ['env']        
+    rules: [
+      {      
+        // Babel Loader
+        test: /\.js?$/,        
+        exclude: /node_modules/,        
+        loader: 'babel-loader',        
+        query: {           
+          presets: ['env']        
+        }
+      },
+      {
+        // CSS Loader
+        test:/\.scss$/,
+        use:['style-loader','css-loader','sass-loader']
       }
-    }]
-  }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./dist/"),
+    port: 8080
+  },
 }
